abec9572da7628df592d349c6b9cff02aec8df59
mal-wood <mallorywood413@gmail.com>
2016-07-06 09:56:49 -0400

    Add basic list of questions and their assigned variables

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+print "What is your name?"
+name = gets.chomp
+
+print "How old are you?"
+age = gets.to_i
+
+print "What year were you born?"
+year_born = gets.chomp
+
+print "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+print "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
