f87e587315e234dc30873ac5c5c2e1a8593e944a
mal-wood <mallorywood413@gmail.com>
2016-07-06 21:03:52 -0400

    Make final edits

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,24 +1,37 @@
- def hamster_adoption
+def hamster_adoption
  puts "What is your name?"
- name = gets.chomp
+ 	name = gets.chomp
  puts "What is the hamster's volume (1-10)?"
- volume = gets.chomp.to_i
+ 	volume = gets.chomp.to_i
  puts "What is the fur color?"
- color = gets.chomp
+ 	color = gets.chomp
  puts "Is it a good candidate for adoption? (y/n)"
- good_candidate = gets.chomp
- 	if good_candidate == "y"
- 		good_candidate = true
- 	else
- 		good_candidate = false
- 	end
+ 	good_candidate = gets.chomp
+   		if good_candidate == "y"
+   		good_candidate = true
+   		else
+   		good_candidate = false
+   		end
  puts "What is the estimated age?"
- age = gets.chomp.to_i
-
- if age == ""
-   age = nil
- end
- puts age
+ 		age = gets.chomp
+   		if age.length == 0
+   		  age = nil
+   		else
+   		  age = age.to_i
+   		end
+ puts "The hamster's name is #{name}."
+ puts "The hamster's volume is #{volume}."
+ puts "The fur color is #{color}."
+ 	if good_candidate == true
+ 	  puts "The hamster is a good candidate."
+ 	else
+ 	  puts "The hamster is not a good candidate."
+ 	end
+ 	if age == nil
+ 	  puts "We don't know the age."
+ 	else
+ 	  puts "The age is #{age}."
+ 	end
 end
 
 hamster_adoption
