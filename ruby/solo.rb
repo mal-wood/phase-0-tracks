650f8bed49298a0f3abe627bc441371304d8fc10
mal-wood <mallorywood413@gmail.com>
2016-07-22 18:31:35 -0400

    Begin work on loop

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,25 +1,28 @@
+full_name= ""
+while full_name != "exit"
 print "What is your name?"
 full_name = gets.chomp
-new_array = full_name.split(" ")
-new_array[0], new_array[1] = new_array[1], new_array[0]
-full_name= new_array.join(" ")
-letters= full_name.split('')
-
-vowels = ["a","A","e","E","i","I","o","O","u","U"]
-consonants =["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","y","Y","z","Z"]
-
-letters.map! do |letter|
-	if vowels.include? letter
-		new_index= vowels.index(letter) + 2
-		new_letter= vowels[new_index]
-		letter=new_letter
-	elsif 
-		consonants.include? letter
-		new_index= consonants.index(letter) + 2
-		new_letter=consonants[new_index]
-		letter=new_letter
-	elsif letter == ' '
-		letter = ' '
+	new_array = full_name.split(" ")
+	new_array[0], new_array[1] = new_array[1], new_array[0]
+	full_name= new_array.join(" ")
+	letters= full_name.split('')
+	
+	vowels = ["a","A","e","E","i","I","o","O","u","U"]
+	consonants =["b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","y","Y","z","Z"]
+	
+	letters.map! do |letter|
+		if vowels.include? letter
+			new_index= vowels.index(letter) + 2
+			new_letter= vowels[new_index]
+			letter=new_letter
+		elsif 
+			consonants.include? letter
+			new_index= consonants.index(letter) + 2
+			new_letter=consonants[new_index]
+			letter=new_letter
+		elsif letter == ' '
+			letter = ' '
+		end
 	end
+	 p letters.join
 end
-letters.join
