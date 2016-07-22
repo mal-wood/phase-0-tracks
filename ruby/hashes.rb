3b263fe4663c0619b37b6dc7662e4bbfba77d5f7
mal-wood <mallorywood413@gmail.com>
2016-07-22 15:22:42 -0400

    Add final touches on wallpaper and paint preferences

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -24,11 +24,49 @@ number_of_children = gets.chomp.to_i
 print "Wants walkthrough (y/n):"
 walkthrough = gets.chomp
 
+# Ask user if they like a certain kind of wallpaper
+#If they do like the wallpaper, print it to the array
+# If not, do not include in the array
+
+kind_of_wallpaper = ["polka dot", "striped", "rainbow", "floral"]
+
+def wallpaper_generator(array)
+	new_array = []
+array.map! do |style|
+print "Do you like #{style} style of wallpaper? (y/n)"
+	wallpaper_pref = gets.chomp
+	if wallpaper_pref == "y"
+		new_array << style
+	end	
+end
+new_array
+end
+
+paper_pref = wallpaper_generator(kind_of_wallpaper)
+
+paint_colors = ["blue", "red", "green", "yellow"]
+
+def paint_generator(array)
+	positive_array = []
+	array.map! do |color|
+	print "Do you like the paint color #{color}? (y/n)"
+		paint_pref = gets.chomp
+		if paint_pref == "y"
+			positive_array << color
+		end
+	end
+
+	positive_array
+end
+
+color_pref = paint_generator(paint_colors)
 
 client_info[:name] = name
 client_info[:age] = age
 client_info[:number_of_children] = number_of_children
 client_info[:walkthrough] = walkthrough
+client_info[:wallpaper_pref] = paper_pref
+client_info[:paint_pref] = color_pref
 
 p client_info
 
@@ -36,7 +74,7 @@ print "Does the above information look correct? If not, enter in key of value yo
 mistake = gets.chomp
 
 
-if mistake == "name" || mistake == "age" || mistake == "number_of_children" || mistake == "decor_theme" || mistake == "walkthrough"
+if mistake == "name" || mistake == "age" || mistake == "number_of_children" || mistake == "decor_theme" || mistake == "walkthrough" || mistake == "wallpaper_pref"
 	key_to_update = mistake.to_sym
 	print "Please update new value:"
 	new_value = gets.chomp
