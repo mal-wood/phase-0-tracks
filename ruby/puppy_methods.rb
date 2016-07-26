a4f0f1304e95adf0a704d1fe7d0ac04468b45d0e
mal-wood <mallorywood413@gmail.com>
2016-07-26 14:34:49 -0400

    Finish puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,40 +1,70 @@
-class Puppy
+# class Puppy
   
-   def initialize
-    puts "Initializing new puppy instance ..."
-  end
+#    def initialize
+#     puts "Initializing new puppy instance ..."
+#   end
 
-  # Make the puppy fetch a toy
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+#   # Make the puppy fetch a toy
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+  
+#   # Make the puppy speak
+#   def speak(integer)
+#     puts "Woof!" * integer
+#   end
+  
+#   # Make the puppy roll over
+#   def roll_over
+#     puts "*rolls over*"
+#   end
+  
+#   # Convert dog age to human years
+#   def dog_years(human_years)
+#     puts human_years * 7
+#   end
+#   # Make the puppy jump
+#   def jump
+#     puts "*jumps*"
+#   end
+  
+# end
+
+# # Driver code
+# lab = Puppy.new
+# lab.fetch("ball")
+# lab.speak(5)
+# lab.roll_over
+# lab.dog_years(3)
+# lab.jump
+
+class Elephants
+  def initialize
+    @size = "Large"
+    @color = "Grey"
+    puts "Initializing a new elephant..."
   end
   
-  # Make the puppy speak
-  def speak(integer)
-    puts "Woof!" * integer
+  # Make the elephant stomp
+  def stomp(steps)
+    puts "The elephant stomped #{steps} times."
   end
   
-  # Make the puppy roll over
-  def roll_over
-    puts "*rolls over*"
+  # Make the elephant spray with its trunk
+  def spray
+    puts "The elephant just sprayed you with its trunk!"
   end
-  
-  # Convert dog age to human years
-  def dog_years(human_years)
-    puts human_years * 7
-  end
-  # Make the puppy jump
-  def jump
-    puts "*jumps*"
-  end
-  
 end
 
-# Driver code
-lab = Puppy.new
-lab.fetch("ball")
-lab.speak(5)
-lab.roll_over
-lab.dog_years(3)
-lab.jump
+# Make 50 elephants
+herd = []
+50.times do
+  herd << Elephants.new
+end
+
+# Have each elephant stomp and spray
+herd.each do |elephant|
+  elephant.stomp(2)
+  elephant.spray
+ end
