faaf65b1104871344d4401ede1390bea901ea366
mal-wood <mallorywood413@gmail.com>
2016-07-26 19:54:10 -0400

    Add additional driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,20 +1,47 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		puts "Santa's gender: #{gender}"
+		puts "Santa's ethnicity: #{ethnicity}"
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
+
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end 
 
+
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
 	end
 
+
+	def celebrate_birthday
+		puts @age = @age + 1
+	end
+
+
+	def get_mad_at
 end 
 
 #Testing above methods with driver code 
-kelsey = Santa.new
-kelsey.speak
-kelsey.eat_milk_and_cookies("sugar cookie")
+# kelsey = Santa.new
+# kelsey.speak
+# kelsey.eat_milk_and_cookies("sugar cookie")
+
+# santas = []
+# genders = ["nonbinary", "agender", "genderqueer", "female"]
+# ethnicities = ["black", "Latinx", "white", "prefer not to disclose"]
+
+# genders.length.times do |item|
+# 	santas << Santa.new(genders[item], ethnicities[item])
+# end
+
+happy_santa = Santa.new("agender", "Chinese")
+happy_santa.eat_milk_and_cookies("lemon cookies")
+happy_santa.celebrate_birthday
