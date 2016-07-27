c80e8a39fc3caee6510cc3d35694ee384641030a
mal-wood <mallorywood413@gmail.com>
2016-07-27 18:01:58 -0400

    Add new module example and code to flight file

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,33 @@
+# module Shout
+
+#     def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   	def self.yell_happily(words)
+#   	words + " Weeeee" + " :D"
+#   end
+# end
+
+# puts Shout.yell_angrily("Donald Trump")
+# puts Shout.yell_happily("Rainbows and unicorns and butterflies")
+
 module Shout
+	def yell(words)
+		words + "!!!!"
+	end 
+end 
 
-    def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+class Person 
+	include Shout
+end 
 
-  	def self.yell_happily(words)
-  	words + " Weeeee" + " :D"
-  end
-end
+class Megaphone
+	include Shout
+end 
 
-puts Shout.yell_angrily("Donald Trump")
-puts Shout.yell_happily("Rainbows and unicorns and butterflies")
+human = Person.new
+puts human.yell("I am a person")
+
+blue_megaphone = Megaphone.new 
+puts blue_megaphone.yell("I am an inanimate object")
