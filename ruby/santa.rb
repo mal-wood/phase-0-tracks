ddb38a9efe6f414f7d854de1288a241fae042031
mal-wood <mallorywood413@gmail.com>
2016-07-27 16:06:37 -0400

    Add new driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,8 @@
 class Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
 
-	def initialize(gender, ethnicity)
+	def initialize(gender, ethnicity, name)
 		puts "Initializing Santa instance ..."
 		puts "Santa's gender: #{gender}"
 		puts "Santa's ethnicity: #{ethnicity}"
@@ -10,6 +12,16 @@ class Santa
 		@age = 0
 	end
 
+	def celebrate_birthday
+		@age= @age + 1
+		puts @age
+	end
+
+	def get_mad_at(name)
+		@name = name
+		@reindeer_ranking.insert(8, @reindeer_ranking.delete_at(@reindeer_ranking.index(name)))
+		p @reindeer_ranking
+	end
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -20,13 +32,6 @@ class Santa
 		puts "That was a good xw#{cookie_type}!"
 	end
 
-	def age
-		@age
-	end
-
-	def ethnicity
-		@ethnicity
-	end
 end 
 
 #Testing above methods with driver code 
@@ -42,8 +47,9 @@ end
 # 	santas << Santa.new(genders[item], ethnicities[item])
 # end
 
-happy_santa = Santa.new("agender", "Chinese")
+happy_santa = Santa.new("agender", "Chinese", "Rudolph")
 happy_santa.eat_milk_and_cookies("lemon cookies")
 happy_santa.celebrate_birthday
-
-
+happy_santa.get_mad_at("Blitzen")
+p happy_santa.gender=("bigender")
+p "Santa is #{happy_santa.age} years old and is #{happy_santa.ethnicity}"
