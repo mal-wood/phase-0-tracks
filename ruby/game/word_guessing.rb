509be06798ed2468ef5e839fdf7885e54e1a377b
mal-wood <mallorywood413@gmail.com>
2016-07-30 17:24:51 -0400

    Move files to correct directory and add pseudocode to program file

---
+++ ruby/game/word_guessing.rb
@@ -0,0 +1,5 @@
+#Class for word-guessing game 
+	#Initialize the game with attributes: word, guess count, and is over 
+	# A method that takes user input and turns it into a string only containing "_" for each character of the input 
+	# A method that replaces any correctly guessed letter in the string of underscore's
+	
