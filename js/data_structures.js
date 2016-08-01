e86bcf8c0d2432d7baed4ffc62b8f2822ccdb7a7
mal-wood <mallorywood413@gmail.com>
2016-08-01 17:01:52 -0400

    Add horses object with names and colors as keys/values

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,6 +4,14 @@ var names = ["Smokey", "Patches", "Captain", "Trotty"];
 colors.push("purple");
 names.push("Maney");
 
-console.log(colors)
-console.log(names)
+console.log(colors);
+console.log(names);
 
+var horses = {};
+for (var i = 0; i < colors.length; i++) {
+	horseNames = names[i];
+	horseColor = colors[i];
+	horses[horseNames] = horseColor;
+};
+
+console.log(horses);
