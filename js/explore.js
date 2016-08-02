56ba270910e12f97241ae0a4096dd7778cb652f6
mal-wood <mallorywood413@gmail.com>
2016-08-01 21:45:56 -0400

    Add function to reverse a phrase

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// Split the phrase that is entered
+// Reverse that array of letters
+// Join the phrase back together 
+
+function reverse(string) {
+	return string.split('').reverse().join('');
+}
