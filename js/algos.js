f56cfb4b5ff4af69747b04e06b7c7a329684541f
mal-wood <mallorywood413@gmail.com>
2016-08-03 19:34:01 -0400

    Add first algoritm

---
+++ js/algos.js
@@ -0,0 +1,18 @@
+// take array of words
+// calculate length of each word in the array 
+// return the word that is the longest
+
+var longestWord = function(array){
+for (var i = 0; i < array.length; i++) {
+		var winner = 0;
+		var longest_word;
+		if (array[i].length > winner){
+			var winner = array[i].length
+			var longest_word = array[i]
+			console.log(longest_word)
+		}
+		
+	}   
+}
+
+longestWord(["bird","hat","pennsylvania"])
