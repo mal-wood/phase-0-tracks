a3b004656de9f07ed2cbc8831a540f936759508f
mal-wood <mallorywood413@gmail.com>
2016-08-04 23:22:14 -0400

    Add some new function ideas

--- js/algos.js
+++ js/algos.js
@@ -2,17 +2,40 @@
 // calculate length of each word in the array 
 // return the word that is the longest
 
-var longestWord = function(array){
-for (var i = 0; i < array.length; i++) {
-		var winner = 0;
-		var longest_word;
-		if (array[i].length > winner){
-			var winner = array[i].length
-			var longest_word = array[i]
-			console.log(longest_word)
-		}
-		
-	}   
-}
+// var longestWord = function(array){
+// 	var winner_length = 0;
+// 	var winner = ''
+// for (var i = 0; i < array.length; i++) {
+// 		if (array[i].length > winner_length.length){
+// 			winner_length = array[i].length;
+// 			winner = array[i];
+// 		}
+// 	}   
+// 	console.log(winner)
+// }
 
-longestWord(["bird","hat","pennsylvania"]) //
+// longestWord(["bird", "house", "hat"])
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+// write method that accepts two hashes
+// if a key value pair in one object mathches a key value pair in another, return true 
+// if a key value pair in one object does not match any in the other, return false
+
+// var keyvalueMatch = function(hash_one, hash_two){
+// 	for (var key in hash_one) {
+// 		if hash_one[i]
+// 	}
+// }
+
+// person_one = {name: "John", age: 33, hometown: "Austin"};
+// person_two = {name: "Maya", age: 20, hometown: "Austin"};
+// keyvalueMatch(person_one, person_two)
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+// write a function that takes an integer
+// generate an array that contains that many strings of random words
+// words should have min of 1 characters and max of 10 
