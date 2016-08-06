fd0c0154972e208c8a07d38fa8a73d9e75349135
mal-wood <mallorywood413@gmail.com>
2016-08-05 22:39:52 -0400

    Add pseudocode for third method

--- js/algos.js
+++ js/algos.js
@@ -1,36 +1,40 @@
-// take array of words
-// calculate length of each word in the array 
-// return the word that is the longest
+// // take array of words
+// // calculate length of each word in the array 
+// // return the word that is the longest
 
-// var longestWord = function(array){
-// 	var winner_length = 0;
-// 	var winner = ''
-// for (var i = 0; i < array.length; i++) {
-// 		if (array[i].length > winner_length.length){
-// 			winner_length = array[i].length;
-// 			winner = array[i];
-// 		}
-// 	}   
-// 	console.log(winner)
-// }
+var longestWord = function(array){
+	var winner_length = 0;
+	var winner = ''
+		for (var i = 0; i < array.length; i++) {
+			if (array[i].length > winner_length.length){
+				winner_length = array[i].length;
+				winner = array[i];
+			}
+		}   
+	console.log(winner)
+}
 
-// longestWord(["bird", "house", "hat"])
+longestWord(["bird", "house", "hat"])
 
 //////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 
-// write method that accepts two hashes
-// if a key value pair in one object mathches a key value pair in another, return true 
-// if a key value pair in one object does not match any in the other, return false
+// // write method that accepts two hashes
+// // if a key value pair in one object mathches a key value pair in another, return true 
+// // if a key value pair in one object does not match any in the other, return false
 
 // var keyvalueMatch = function(hash_one, hash_two){
-// 	for (var key in hash_one) {
-// 		if hash_one[i]
+// 	for (var label in hash_one){
+// 		for (var label_two in hash_two) {
+// 			if hash_one[label] = hash_two[label]
+// 				console.log('Match!')
+// 		}
 // 	}
 // }
 
-// person_one = {name: "John", age: 33, hometown: "Austin"};
-// person_two = {name: "Maya", age: 20, hometown: "Austin"};
+// var person_one = {name: "John", age: 33, hometown: "Austin"};
+// var person_two = {name: "Maya", age: 20, hometown: "Austin"};
+
 // keyvalueMatch(person_one, person_two)
 
 //////////////////////////////////////////////////////////////
