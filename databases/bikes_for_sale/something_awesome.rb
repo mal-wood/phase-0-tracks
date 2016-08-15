e6ff677d8cec9d0de0c9c8dac2e4d7b507ed2c5c
mal-wood <mallorywood413@gmail.com>
2016-08-14 20:29:17 -0600

    Pseudocode and add gems

---
+++ databases/bikes_for_sale/something_awesome.rb
@@ -0,0 +1,9 @@
+# Ideas for program 
+## User is welcomed and given a list of 10 randomly generated cities
+## The user is then asked which city they would like to visit on a whim 
+## The user chooses by typing the ID of the city 
+## Program prints a statement about the user's upcoming trip 
+
+# require necessary gems
+	require 'sqlite3'
+	require 'faker'
